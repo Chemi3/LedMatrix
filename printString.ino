9549b7f77a375b0cf27e1ddee4a23ceeec758cc4
Chema Martin Ramos <28399333+Chemi3@users.noreply.github.com>
2020-08-13 14:03:21 +0200

    Update printString.ino

--- printString.ino
+++ printString.ino
@@ -1,18 +1,19 @@
 #define SIZE 32 
-void display(String frase)
+void display(String sentence)
 {
-  int tam = frase.length();
+  String checkStr = "1ifl ";
+  int tam = sentence.length();
   int min = -(6 * tam);
   for (int a = SIZE; a >= min; a--)
   {
     int posX = a;
     for (int i = 0; i < tam; i++)
     {
-      if (checkStr.indexOf(frase[i]) != -1)
+      if (checkStr.indexOf(sentence[i]) != -1)
         posX--;
       if (posX <= 32)
-        matrix.drawChar(posX, 0, frase[i], 1, 0, 1);
-      if (checkStr.indexOf(frase[i]) != -1)
+        matrix.drawChar(posX, 0, sentence[i], 1, 0, 1);
+      if (checkStr.indexOf(sentence[i]) != -1)
         posX += 5;
       else
         posX += 6;
